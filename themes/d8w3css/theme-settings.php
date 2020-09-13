39462b2b15d11da269a546626eb701f3b7c57a9b
www-linux <git@linux-www.com>
2020-09-13 22:50:54 +0200

    +
    Co-Authored-By: null <linux-www@users.noreply.github.com>

---
+++ themes/d8w3css/theme-settings.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * @file
+ * Drupal8 W3CSS Theme.theme.
+ *
+ * Filename:     drupal8_w3css_theme.theme
+ * Website:      http://www.flashwebcenter.com
+ * Description:  template
+ * Author:       Alaa Haddad http://www.alaahaddad.com.
+ */
+
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Implements hook_form_system_theme_settings_alter().
+ */
+function drupal8_w3css_theme_form_system_theme_settings_alter(&$form, FormStateInterface $form_state) {
+
+  // Theme settings files.
+  require_once __DIR__ . '/includes/website_width.inc';
+  require_once __DIR__ . '/includes/match_height.inc';
+  require_once __DIR__ . '/includes/equal_width.inc';
+  require_once __DIR__ . '/includes/predefined_themes.inc';
+  require_once __DIR__ . '/includes/advanced_site_colors.inc';
+  require_once __DIR__ . '/includes/social_links.inc';
+  require_once __DIR__ . '/includes/copyright.inc';
+  require_once __DIR__ . '/includes/credit.inc';
+}
